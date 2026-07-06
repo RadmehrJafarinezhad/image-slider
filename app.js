b52eed613fdd30313b036fdfd35784b4cc5a8c11
Radmehr Jafarinezhad <radmehr.j.66@gmail.com>
2026-07-06 16:32:09 +0330

    refactor: convert slider to ES6 class and add fade/wipe transitions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict"
 
-import createSlider from "./js/slider.js"
+import Slider from "./js/slider.js"
 
 
 const dotContainer = document.getElementById('dot-container');
@@ -18,8 +18,7 @@ async function sliderOneSettings() {
 
     pics = await getDatasFromJSON("sliderImages");
 
-    const slider1 = createSlider();
-    slider1.createImgs();
+    let slider1 = new Slider("img-container",pics,"dot-container","wipe")
 
     document.addEventListener("keydown", slider1.sliderKeyboardControl);
 
@@ -27,15 +26,14 @@ async function sliderOneSettings() {
 
     const right_slider_btn = document.getElementById('right-slider-btn');
 
-    if(!right_slider_btn) console.error("Forbidden: right-slider-btn ID its forbidden.");
+    if(!right_slider_btn) throw new Error("Forbidden:\nright-slider-btn ID its forbidden.");
     else right_slider_btn.addEventListener("click",() => {
         slider1.goNext();
-        slider1.startAutoPlay();
     });
 
     const left_slider_btn = document.getElementById('left-slider-btn')
 
-    if(!left_slider_btn) console.error("Forbidden: right-slider-btn ID its forbidden.");
+    if(!left_slider_btn) throw new Error("Forbidden:\nright-slider-btn ID its forbidden.");
     else left_slider_btn.addEventListener("click",() => {
         slider1.goPrev();
         slider1.startAutoPlay();
@@ -52,7 +50,7 @@ async function getDatasFromJSON(key) {
         let response = await fetch("data.json" );
 
         if(!response.ok){
-            throw new Error("Forbidden : data.json doesn't exist")
+            throw new Error("Forbidden:\ndata.json doesn't exist")
         }
 
         let data = await response.json();
@@ -65,7 +63,3 @@ async function getDatasFromJSON(key) {
 
     }
 }
-
-
-
-mobileNavInit()
--- js/slider.js
+++ js/slider.js
@@ -1,164 +1,255 @@
-import { pics } from "../app.js"
+"use strict"
+
+class Slider {
+
+    root = null
+    count = 0;
+    imageIDs = [];
+    interval = null;
+    #lastTransition = null;
+    #imgCount = null;
+
+    constructor(rootID, pics, dotContainerID, transition) {
+        this.rootID = rootID
+        this.pics = pics
+        this.dotContainerID = dotContainerID
+
+        this.root = document.getElementById(this.rootID);
+        this.dotContainer = document.getElementById(this.dotContainerID);
+
+        this.transition = transition;
+
+        if (this.transition == "fade") {
+
+            this.class = ["opacity-0", "transition-opacity"];
+
+        } else if (this.transition == "wipe") {
+
+            this.class = "translate-x-full";
+            this.#lastTransition = "translate-x-full";
+
+        }
+
+        this.createImgs();
+        this.createDots();
+    }
+
+    createImgs() {
+
+        this.root.innerHTML = "";
+
+        for (let i = 0; i < this.pics.length; i++) {
 
 
-function createSlider() {
-
-    const root = document.getElementById("img-container");
-    let count = 0;
-    let imageIDs = new Array;
-    let interval = null
-
-    if (!root) throw new Error("Forbidden: \nimg-container not found.");
-
-    return {
-
-        createImgs() {
+            const newImage = document.createElement("img");
+            newImage.src = this.pics[i];
+            newImage.setAttribute("id", `img${i}`);
 
 
+            let imgClasses = [
+                "absolute",
+                "top-0",
+                "left-0",
+                "w-[300px]",
+                "h-[200px]",
+                "sm:w-[500px]",
+                "sm:h-[300px]",
 
-            root.innerHTML = "";
+                "object-cover",
+                "duration-1000",
+                "ease-in-out",
+                this.class
+            ].flat();
 
-            for (let i = 0; i < pics.length; i++) {
+            newImage.classList.add(...imgClasses);
 
-                const newImage = document.createElement("img");
-                newImage.src = pics[i];
-                newImage.setAttribute("id", `img${i}`);
-                newImage.classList.add(
-                    "absolute",
-                    "top-0",
-                    "left-0",
-                    "opacity-0",
-                    "w-[300px]",
-                    "h-[200px]",
-                    "sm:w-[500px]",
-                    "sm:h-[300px]",
-                    "rounded-xl",
-                    "object-cover",
-                    "transition-opacity",
-                    "duration-1000",
-                    "ease-in-out",
-                )
+            this.root.appendChild(newImage);
 
-                root.appendChild(newImage);
-
-                imageIDs.push(newImage.id);
-                console.log(imageIDs)
-
-            }
-
-            document.getElementById(imageIDs[0]).classList.remove("opacity-0");
-        },
-        createDots() {
-
-            const dotContainer = document.getElementById("dot-container");
-
-            dotContainer.innerHTML = "";
-
-            for (let i = 0; i < pics.length; i++) {
-                const dot = document.createElement("button");
-
-                let dot_ID = `dot${i}`;
-                dot.addEventListener("click", () => {
-                    slider(i)
-                });
-                dot.classList.add(
-                    "dot",
-                    "h-3",
-                    "w-3",
-                    "sm:w-4",
-                    "sm:h-4",
-                    "m-2",
-                    "bg-white",
-                    "rounded-[100%]",
-                    "shadow",
-                    "shadow-black",
-                    "shadow-lg",
-                    "border-gray-200",
-                    "border-[1px]",
-                    "hover:scale-125",
-                    "transition-transform",
-                    "duration-100"
-                );
-
-                dot.setAttribute("id", dot_ID)
-                dotContainer.appendChild(dot)
-                console.log(dot)
-            }
-            document.getElementById("dot0").classList.add("scale-125");
-        },
-
-        goNext() {
-
-            const current = document.getElementById(imageIDs[count]);
-            current.classList.add("opacity-0");
-
-            if (count < imageIDs.length - 1) {
-
-                count++
-
-            } else {
-                count = 0;
-            }
+            this.imageIDs.push(newImage.id);
+            console.log(this.imageIDs);
 
 
-            document.getElementById(imageIDs[count]).classList.remove("opacity-0");
+        }
 
-            this.updateDots();
+        this.#imgCount = this.imageIDs[this.count];
 
-        },
+        switch (this.transition) {
 
-        goPrev() {
-            const current = document.getElementById(imageIDs[count]);
-            current.classList.add("opacity-0");
+            case "fade":
+                document.getElementById(this.#imgCount).classList.remove("opacity-0");
+                break;
 
-            if (count > 0) {
-                count--
-            } else {
-                count = imageIDs.length - 1;
-            }
-
-
-
-            document.getElementById(imageIDs[count]).classList.remove("opacity-0");
-
-            this.updateDots();
-
-        },
-
-        sliderKeyboardControl(event) {
-
-            if (event.key == "ArrowRight") {
-                this.goNext();
-            }
-
-            else if (event.key == "ArrowLeft") {
-                this.goPrev();
-            }
-
-        },
-        autoPlay() {
-            if (interval) return;
-
-            interval = setInterval(() => this.goNext(), 4000)
-        },
-
-        startAutoPlay() {
-            clearInterval(interval);
-
-            interval = setInterval(() => this.goNext(), 2000);
-        },
-
-        stopAutoPlay() {
-            clearInterval(interval);
-        },
-
-        updateDots() {
-            let prevActiveDot = document.querySelector(".dot.scale-125");
-            if (prevActiveDot) prevActiveDot.classList.remove("scale-125");
-            document.getElementById(`dot${count}`).classList.add("scale-125")
+            case "wipe":
+                document.getElementById(this.#imgCount).classList.replace("translate-x-full", "translate-x-0");
+                break;
 
         }
     }
-}
+    createDots() {
 
-export default createSlider;
+        this.dotContainer.innerHTML = "";
+
+        for (let i = 0; i < this.pics.length; i++) {
+            const dot = document.createElement("button");
+
+            let dot_ID = `dot${i}`;
+            dot.addEventListener("click", () => {
+                this.goToID(i)
+            });
+            dot.classList.add(
+                "dot",
+                "h-3",
+                "w-3",
+                "sm:w-4",
+                "sm:h-4",
+                "m-2",
+                "bg-white",
+                "rounded-[100%]",
+                "shadow",
+                "shadow-black",
+                "shadow-lg",
+                "border-gray-200",
+                "border-[1px]",
+                "hover:scale-125",
+                "transition-transform",
+                "duration-100"
+            );
+
+            dot.setAttribute("id", dot_ID)
+            this.dotContainer.appendChild(dot)
+            console.log(dot)
+        }
+        document.getElementById("dot0").classList.add("scale-125");
+    }
+
+    goToID(value) {
+
+        this.count = value;
+
+        this.slideConfig()
+
+        this.startAutoPlay();
+    }
+    goNext() {
+
+        this.currentTransition(this.transition,"hide","next")
+
+        if (this.count < this.imageIDs.length - 1) {
+
+            this.count++
+
+        } else {
+            this.count = 0;
+        }
+
+        this.currentTransition(this.transition,"show","next")
+        this.slideConfig()
+    }
+
+    goPrev() {
+
+        this.currentTransition(this.transition,"hide","prev");
+
+        if (this.count > 0) {
+            this.count--
+        } else {
+            this.count = this.imageIDs.length - 1;
+        }
+
+        this.currentTransition(this.transition,"show","prev");
+        this.slideConfig()
+
+    }
+
+    sliderKeyboardControl(event) {
+
+        if (event.key == "ArrowRight") {
+            this.goNext();
+        }
+
+        else if (event.key == "ArrowLeft") {
+            this.goPrev();
+        }
+
+    }
+    autoPlay() {
+        if (this.interval) return;
+
+        this.interval = setInterval(() => this.goNext(false), 4000)
+    }
+
+    startAutoPlay() {
+        clearInterval(this.interval);
+
+        this.interval = setInterval(() => this.goNext(), 2000);
+    }
+
+    stopAutoPlay() {
+        clearInterval(this.interval);
+    }
+
+    updateDots() {
+        let prevActiveDot = document.querySelector(".dot.scale-125");
+        if (prevActiveDot) prevActiveDot.classList.remove("scale-125");
+        document.getElementById(`dot${this.count}`).classList.add("scale-125")
+
+    }
+    slideConfig() {
+
+        this.updateDots();
+
+        this.startAutoPlay();
+    }
+    currentTransition(value, action,arrow) {
+
+        this.#imgCount = this.imageIDs[this.count];
+
+        if (value == "fade") {
+
+            if (action == "hide") {
+
+                document.getElementById(this.#imgCount).classList.add("opacity-0");
+
+            }
+
+            if (action == "show") {
+
+                document.getElementById(this.#imgCount).classList.remove("opacity-0");
+
+            }
+
+        } else if (value == "wipe") {
+
+            this.#imgCount = this.imageIDs[this.count];
+
+            switch(action) {
+
+                case "hide":
+                    switch(arrow){
+
+                        case "prev":
+                            document.getElementById(this.#imgCount).classList.replace("translate-x-0","-translate-x-full");
+                            this.#lastTransition = "-translate-x-full";
+                            break;
+
+                        case "next":
+                            document.getElementById(this.#imgCount).classList.replace("translate-x-0","translate-x-full");
+                            this.#lastTransition = "translate-x-full";
+                            break;
+
+                    }
+                    break;
+
+                case "show":
+
+                    const img = document.getElementById(this.#imgCount);
+
+                    document.getElementById(this.#imgCount).classList.remove("-translate-x-full","translate-x-full")
+
+                    document.getElementById(this.#imgCount).classList.add( "translate-x-0");
+                    break;
+            }
+        }
+    }
+}
+export default Slider;
